d2289fe0f7d205078b0173e3e6f3ae0d3ebee7fa
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-08-29 12:18:01 -0600

    add color coding and completed text editing

--- script.js
+++ script.js
@@ -1,49 +1,127 @@
 
 
-var currentTime = moment().format('LLLL');
-$("#currentDay").text(currentTime);
+var currentTime = moment();
+var formattedTime = moment(currentTime).format('LLLL');
+$("#currentDay").text(formattedTime);
+colorCode(currentTime);
 
 setInterval(function () {
-    currentTime = moment().format('LLLL');
-    $("#currentDay").text(currentTime);
+    formattedTime = moment().format('LLLL');
+    $("#currentDay").text(formattedTime);
 
     // call function here to set colors based on time of day
+    colorCode(currentTime);
 
-    // \/ get rid of this before completing \/
-    console.log("ran interval")
-}, (1000 * 60) * 1);
+    // console.log("ran interval")
+}, (1000 * 60) * .1);
 
+
+
+
+
+function colorCode(cTime) {
+
+    cTime = moment(cTime).format("HH");
+    // console.log(cTime);
+
+    $(".time-block-content").each(function () {
+        var testContent = $(this).parent().children(".hour").text().trim().toLowerCase();
+        let pmMatch = testContent.match(/pm/);
+        let timeValue = parseInt(testContent);
+        if (pmMatch && timeValue != 12) {
+            // console.log(testContent + " is in the afternoon")
+            timeValue += 12
+        }
+
+        if (timeValue < cTime) {
+            // console.log("timeValue is less than cTime (24Hour)")
+            $(this).addClass("past");
+        }
+        else if (timeValue == cTime) {
+            // console.log("timeValue is equal to cTime (24Hour)")
+            $(this).addClass("present");
+        }
+        else {
+            // console.log("timeValue is greater than cTime (24Hour)")
+            $(this).addClass("future");
+        }
+
+        // var testContent = $(this).text().trim();
+        // console.log(testContent);
+        // console.log(timeValue);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+$(".container").on("blur", ".textArea", function () {
+
+    let text = $(this).parent().children(".textArea")
+        .val()
+        .trim();
+
+    // let currentClass = $(this)
+    //     .attr("class");
+
+    // currentClass = currentClass.replace("col-10", "")
+    // currentClass = currentClass.relplace("textArea", "")
+
+    let textInput = $("<div>")
+        .addClass("col-10 pt-2 time-block-content")
+        .text(text);
+
+    $(this).parent().children(".textArea").replaceWith(textInput);
+
+    colorCode(currentTime);
+});
 
 $(".container").on("click", ".time-block-content", function () {
-    var text = $(this)
+    let text = $(this)
         .text()
         .trim();
-    var textInput = $("<textarea>")
-        .addClass("col-10 bg-light textArea")
+
+    let currentClass = $(this)
+        .attr("class");
+
+    currentClass = currentClass.replace("col-10 pt-2 time-block-content", "")
+
+    let textInput = $("<textarea>")
+        .addClass("col-10 textArea")
+        .addClass(currentClass)
         .val(text);
     $(this).replaceWith(textInput);
+    $(".textArea").trigger("focus");
 
 
-
-    textInput.trigger("focus");
-    console.log("clicked");
-    console.log(text);
-    console.log(textInput);
-    console.log(this);
-    console.log(event);
 });
 
 $(".container").on("click", ".saveBtn", function () {
 
-    var test = $(this).parent().children(".textArea")
-        .val()
-        .trim();
+    // let text = $(this).parent().children(".textArea")
+    //     .val()
+    //     .trim();
 
-    var textInput = $("<div>")
-        .addClass("col-10 bg-light pt-2 time-block-content")
-        .text(test);
+    // let currentClass = $(this)
+    //     .attr("class");
 
-    $(this).parent().children(".textArea").replaceWith(textInput);
+    // currentClass = currentClass.replace("col-10", "")
+    // currentClass = currentClass.relplace("textArea", "")
+
+    // let textInput = $("<div>")
+    //     .addClass("col-10 pt-2 time-block-content")
+    //     .text(text);
+
+    // $(this).parent().children(".textArea").replaceWith(textInput);
+
+    // colorCode(currentTime);
 
     // $(this).closest(".textArea").replaceWith(textInput);
 
@@ -66,12 +144,10 @@ $(".container").on("click", ".saveBtn", function () {
     // console.log("clicked")
     // console.log(text)
     // console.log(textInput)
-    console.log(this);
+    // console.log(this);
     // console.log("next row is text area");
     // console.log($(this).closest("textarea"));
     // console.log(event);
     // console.log(test);
     // console.log(test2);
 });
-
-
